417e69d3ae3bc337db865b07b34ec2a90be4c3c0
aldoavalos0909 <aldoavalos0909@gmail.com>
2024-05-09 10:25:06 -0700

    Inicio del proyecto de asesorias, se creo los activitis con sus respectivos fragments

---
+++ Asesoria/app/src/main/java/mx/edu/uthermosillo/a19311900/asesoria/OnBoardingFragment.kt
@@ -0,0 +1,18 @@
+package mx.edu.uthermosillo.a19311900.asesoria
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OnBoardingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+       val view:View = inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        return view
+    }
+}
